d344df314f44bf0406c848abfd4dcf3ff02b1f05
spvagelis <spirou.vagelis@gmail.com>
2025-02-20 14:09:08 +0200

    Minimum iOS version 15

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let swiftSettings: Array<SwiftSetting> = [
 let package = Package(
     name: "CocoaLumberjack",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
