1bc21267219e24a8cd4fb82629686e7e855ee2c4
spvagelis <spirou.vagelis@gmail.com>
2025-02-20 11:47:05 +0200

    Convert CocoaLumberjack to dynamic

--- Package.swift
+++ Package.swift
@@ -17,22 +17,21 @@ let swiftSettings: Array<SwiftSetting> = [
 let package = Package(
     name: "CocoaLumberjack",
     platforms: [
-        .macOS(.v10_13),
-        .iOS(.v12),
-        .tvOS(.v12),
-        .watchOS(.v5),
-        .visionOS(.v1),
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CocoaLumberjack",
+            type: .dynamic,
             targets: ["CocoaLumberjack"]),
         .library(
             name: "CocoaLumberjackSwift",
+            type: .dynamic,
             targets: ["CocoaLumberjackSwift"]),
         .library(
             name: "CocoaLumberjackSwiftLogBackend",
+            type: .dynamic,
             targets: ["CocoaLumberjackSwiftLogBackend"]),
     ],
     dependencies: [
