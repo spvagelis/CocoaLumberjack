0c43c6510a0a63ae7846624978e05eeca7d56b31
spvagelis <spirou.vagelis@gmail.com>
2025-02-21 13:36:46 +0200

    Added Package.swift files for swift 5.8 and swift 5.9

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,64 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CocoaLumberjack",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CocoaLumberjack",
+            type: .dynamic,
+            targets: ["CocoaLumberjack"]),
+        .library(
+            name: "CocoaLumberjackSwift",
+            type: .dynamic,
+            targets: ["CocoaLumberjackSwift"]),
+        .library(
+            name: "CocoaLumberjackSwiftLogBackend",
+            type: .dynamic,
+            targets: ["CocoaLumberjackSwiftLogBackend"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CocoaLumberjack",
+            exclude: ["Supporting Files"],
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
+            ]),
+        .target(
+            name: "CocoaLumberjackSwiftSupport",
+            dependencies: ["CocoaLumberjack"]),
+        .target(
+            name: "CocoaLumberjackSwift",
+            dependencies: [
+                "CocoaLumberjack",
+                "CocoaLumberjackSwiftSupport",
+            ],
+            exclude: ["Supporting Files"]),
+        .target(
+            name: "CocoaLumberjackSwiftLogBackend",
+            dependencies: [
+                "CocoaLumberjack",
+                .product(name: "Logging", package: "swift-log"),
+            ]),
+        .testTarget(
+            name: "CocoaLumberjackTests",
+            dependencies: ["CocoaLumberjack"]),
+        .testTarget(
+            name: "CocoaLumberjackSwiftTests",
+            dependencies: ["CocoaLumberjackSwift"]),
+        .testTarget(
+            name: "CocoaLumberjackSwiftLogBackendTests",
+            dependencies: ["CocoaLumberjackSwiftLogBackend"]),
+    ]
+)
